dae6a3dc4caa2a06d0022e439f7496a5ad06401f
Grant Richmond <mail@grant.codes>
2018-10-31 00:08:28 +0100

    update a bunch of stuff to get ready for npm publish

--- index.js
+++ index.js
@@ -3,31 +3,7 @@ const Collection = require('./lib/db')
 const config = require('./lib/config')
 const generateSearch = require('./lib/generate-search')
 const { use } = require('./lib/plugins')
-let micropubRouter = null
-let init = async () => {}
-
-if (isNode) {
-  micropubRouter = require('./lib/router')
-  init = async () => {
-    const collection = await Collection.get()
-    const express = require('express')
-    const app = express()
-    app.use('/', micropubRouter)
-    if (!config.get('mediaBaseUrl')) {
-      config.set('mediaBaseUrl', config.get('endpointBaseUrl') + '/static')
-      app.static('/static', config.get('mediaDir'))
-    }
-    app.listen(config.get('port'), () => {
-      console.log(`Running in standalone mode on port ${config.get('port')}`)
-      console.log('Micropub Endpoint:', config.get('endpointBaseUrl') + '/')
-      console.log('Media Endpoint:', config.get('endpointBaseUrl') + '/media')
-      console.log(
-        'Media folder url:',
-        config.get('endpointBaseUrl') + '/static'
-      )
-    })
-  }
-}
+const micropubRouter = require('./lib/router')
 
 const requiredOptions = [
   'port',
@@ -42,12 +18,6 @@ const requiredOptions = [
   'dbPassword',
   'tokenEndpoint',
 ]
-
-if (isNode && require && require.main === module) {
-  // Running directly, not in another application
-  config.required(requiredOptions)
-  init()
-}
 
 module.exports = (options = {}) => {
   try {
